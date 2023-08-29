95cea057ab3dab7e404ca3d7b2bc85406d962ce7
Guilherme Siqueira <guilherme.siqueira@gpmp.com.br>
2023-08-29 11:16:22 -0300

    style.css

---
+++ ProjetoInfusion/script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const btnMenu = document.querySelector("header__btnMenu");
+
+window.addEventListener("load", () => {
+  body.classList.remove("no-js");
+  body.classList.add("js");
+});
